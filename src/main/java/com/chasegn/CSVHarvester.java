2f9ddf776a84c89a0a5735510513f1094adc9e88
Galen Chase <chasegn@gmail.com>
2021-03-07 13:16:26 -0700

    Implement CSV parsing. Perform record list-building through stream traversal. Initial writeFiles implementation to provide simple result output.

--- src/main/java/com/chasegn/CSVHarvester.java
+++ src/main/java/com/chasegn/CSVHarvester.java
@@ -1,22 +1,34 @@
 package com.chasegn;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class CSVHarvester {
-    Map<String, List<EnrollmentRecord>> insuranceCompanyRecords;
+    Map<String, Map<String, EnrollmentRecord>> insuranceCompanyRecords;
 
     public CSVHarvester() {
         insuranceCompanyRecords = new HashMap<>();
     }
 
-    public void readCSV() {
+    public void readCSV(Stream<String> filestream) {
+        filestream.forEach(s -> {
+            String[] split = s.split("\\s*,\\s*");
+            EnrollmentRecord record = new EnrollmentRecord(split[0], split[1], split[2], Integer.parseInt(split[3]), split[4]);
 
+            insuranceCompanyRecords
+                    .computeIfAbsent(record.getInsuranceCompany(), company -> new HashMap<>())                      // if company hasn't been encountered, add new map to hold those records
+                    .merge(record.getUserId(), record, (v1, v2) -> v1.getVersion() > v2.getVersion() ? v1 : v2);    // determine which record to keep based on higher version number
+        });
     }
 
     public void writeFiles() {
-
+        for (Map<String, EnrollmentRecord> submap : insuranceCompanyRecords.values()) {
+            for (Map.Entry<String, EnrollmentRecord> entry : submap.entrySet()) {
+                System.out.println(entry.getValue().toString());
+            }
+        }
     }
 
 
@@ -26,6 +38,13 @@ public class CSVHarvester {
     public static void main(String[] args) {
         CSVHarvester harvester = new CSVHarvester();
 
+        try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/records.csv"))) {
+            harvester.readCSV(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        harvester.writeFiles();
 
     }
 }
